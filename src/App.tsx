1065ec855c855dbfb9c73759333256703ccb1c0d
Matchobas <maalexandria@hotmail.com>
2022-05-25 22:17:24 -0300

    Created first HTML and CSS structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import suitcaseImg from './assets/suitcaseMoney.jpeg';
+
 export function App() {
   return (
-    <div>Hello World</div>
+    <div className="h-screen flex flex-col items-center justify-center text-center">
+      <h2 className="text-white text-7xl p-4">Timer</h2>
+      <img src={suitcaseImg} />
+      <form className="py-6">
+        <input className="rounded-l h-8 p-2" type="number" placeholder="Type your bet here"></input>
+        <button className="px-4 h-8 bg-purple-700 text-white rounded-r">Enviar</button>
+      </form>
+    </div>
   )
 }
