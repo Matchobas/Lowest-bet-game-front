4dd8740fac0601acb260ce70918a3a3953bfe73b
Matchobas <maalexandria@hotmail.com>
2022-05-27 00:37:59 -0300

    Reading data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import suitcaseImg from './assets/suitcaseMoney.jpeg';
+import { api } from './api';
+import { useEffect, useState } from 'react';
 
 export function App() {
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
+  useEffect(() => {
+    api.get(`auction/95360d67-e29e-4b3f-b641-876f09e4ee76`)
+    .then((response) => {
+      const time = new Date(response.data.end_time);
+      setEndDate(time);
+    })
+  })
+
   return (
     <div className="h-screen flex flex-col items-center justify-center text-center">
-      <h2 className="text-white text-7xl p-4">Timer</h2>
+      <h2 className="text-white text-7xl p-4">{String(endDate)}</h2>
       <img src={suitcaseImg} />
       <form className="py-6">
         <input className="rounded-l h-8 p-2" type="number" placeholder="Type your bet here"></input>
