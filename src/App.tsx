633f8388db663bdff06546b90c1e9414ed9dadbe
Matchobas <maalexandria@hotmail.com>
2022-05-28 21:47:47 -0300

    Creating new bet, but without custom value

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import suitcaseImg from './assets/suitcaseMoney.jpeg';
 import { api } from './api';
-import { useEffect, useState } from 'react';
+import { FormEvent, useCallback, useEffect, useState } from 'react';
 
 export function App() {
   const [endDate, setEndDate] = useState<Date>(new Date());
   const [remainDate, setRemainDate] = useState<Date>(new Date());
+  const [betValue, setBetValue] = useState<number>();
 
   function countdown(date: Date): string {
     const timer = date.toLocaleString("en-GB", {
@@ -16,14 +17,42 @@ export function App() {
     return timer;
   }
 
-  useEffect(() => {
-    const endTime = endDate.getTime();
-    const remainingTime = new Date(endTime - new Date().getTime());
+  function handleBetForm(event: FormEvent) {
+    event.preventDefault();
+    // const data = new FormData();
+    // data.append('auctionId', '9cc788f6-7c0f-4dc1-807a-24c8851fbc7a');
+    // data.append('value', '30');
+    // data.append('username', 'Jeff');
 
-    setRemainDate(remainingTime);
+    try {
+      api.post('bets', {
+        auctionId: '9cc788f6-7c0f-4dc1-807a-24c8851fbc7a',
+        value: 30,
+        username: 'Jeff',
+      }).then((response) => {console.log(response)});
+
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  }
+
+  // Changing the time date to create the countdown timer
+  useEffect(() => {
+    setInterval(() => {
+      const endTime = endDate.getTime();
+      const remainingTime = new Date(endTime - new Date().getTime());
+
+      console.log('Data de termino: ' + endDate);
+      console.log('Data de início: ' + remainingTime);
+
+      setRemainDate(remainingTime);
+    }, 1000000);
+
+    console.log(remainDate);
 
   }, [remainDate, endDate]);
 
+  // Getting an active auction
   useEffect(() => {
     api.get(`auction/95360d67-e29e-4b3f-b641-876f09e4ee76`)
     .then((response) => {
@@ -36,9 +65,22 @@ export function App() {
     <div className="h-screen flex flex-col items-center justify-center text-center">
       <h2 className="text-white text-7xl p-4">{countdown(remainDate)}</h2>
       <img src={suitcaseImg} />
-      <form className="py-6">
-        <input className="rounded-l h-8 p-2" type="number" placeholder="Type your bet here"></input>
-        <button className="px-4 h-8 bg-purple-700 text-white rounded-r">Enviar</button>
+      <form className="py-6" onSubmit={(event) => handleBetForm(event)}>
+        <input
+          onChange={(props) => {
+            setBetValue(props.target.valueAsNumber);
+          }} 
+          className="rounded-l h-8 p-2 text-purple-900" 
+          type="number" 
+          placeholder="Type your bet here"
+        ></input>
+        <button
+          type="submit" 
+          className="px-4 h-8 bg-purple-700 text-white rounded-r hover:bg-purple-600 hover:transition-colors"
+          // disabled={!betValue}
+        >
+          Enviar
+        </button>
       </form>
     </div>
   )
